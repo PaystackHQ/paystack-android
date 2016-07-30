f0fe375d4f2658b6bd8a23e1868e883523eeb914
ibrahimlawal <ibrahim@lawal.me>
2016-07-30 21:40:10 +0100

    - Update to latest versions of all dependencies - Remove Parceler dependency (may be included in future when they fix the InnerClasses issue)

--- paystack/src/main/java/co/paystack/android/api/model/TokenApiResponse.java
+++ paystack/src/main/java/co/paystack/android/api/model/TokenApiResponse.java
@@ -2,14 +2,11 @@ package co.paystack.android.api.model;
 
 import com.google.gson.annotations.SerializedName;
 
-import org.parceler.Parcel;
-
 import java.io.Serializable;
 
 /**
  * A Response received from the API when a Token Request is made
  */
-@Parcel
 public class TokenApiResponse extends ApiResponse implements Serializable {
 
   @SerializedName("token")
