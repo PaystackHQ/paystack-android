f20df2c594b7176f19f567d3164481bd40059c6a
ibrahimlawal <ibrahim@lawal.me>
2016-08-24 15:56:34 +0100

    Add models for charge and transaction - make Token extend PaystackModel - Charge only allows emails validated by Android utils

---
+++ paystack/src/main/java/co/paystack/android/model/Charge.java
@@ -0,0 +1,54 @@
+package co.paystack.android.model;
+
+import android.util.Patterns;
+
+import co.paystack.android.exceptions.InvalidEmailException;
+
+/**
+ * Created by i on 24/08/2016.
+ */
+public class Charge extends PaystackModel {
+    private Card card;
+    private String pin;
+    private String email;
+    private int amount;
+
+    public Card getCard() {
+        return card;
+    }
+
+    public Charge setCard(Card card) {
+        this.card = card;
+        return this;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public Charge setPin(String pin) {
+        this.pin = pin;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Charge setEmail(String email) {
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            throw new InvalidEmailException(email);
+        }
+        this.email = email;
+        return this;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Charge setAmount(int amount) {
+        this.amount = amount;
+        return this;
+    }
+}
---
+++ paystack/src/main/java/co/paystack/android/model/Transaction.java
@@ -0,0 +1,10 @@
+package co.paystack.android.model;
+
+/**
+ * Created by i on 24/08/2016.
+ */
+public class Transaction extends PaystackModel {
+    public String reference;
+
+    public int trans;
+}
