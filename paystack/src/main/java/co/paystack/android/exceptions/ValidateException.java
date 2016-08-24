8a4ba9eae202164036f1a7950d199f5653c909dd
ibrahimlawal <ibrahim@lawal.me>
2016-08-24 15:35:48 +0100

    Add Exceptions types for charge and validate - Removed unused TransactionException class

---
+++ paystack/src/main/java/co/paystack/android/exceptions/ValidateException.java
@@ -0,0 +1,10 @@
+package co.paystack.android.exceptions;
+
+/**
+ * Created by i on 24/08/2016.
+ */
+public class ValidateException extends PaystackException {
+    public ValidateException(String message) {
+        super(message);
+    }
+}
