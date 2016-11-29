30638f0b3355b2031ceaad3cd1929f3ade37cf45
ibrahimlawal <ibrahim@lawal.me>
2016-11-29 18:06:03 +0100

    [swap] Use Paystack's PinPad to collect OTP

--- paystack/src/main/java/co/paystack/android/ui/OtpActivity.java
+++ paystack/src/main/java/co/paystack/android/ui/OtpActivity.java
@@ -2,40 +2,36 @@ package co.paystack.android.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import co.paystack.android.R;
+import co.paystack.android.design.widget.PinPadView;
 
 public class OtpActivity extends Activity {
 
-    private Button continueButton;
-    private EditText otpEntryView;
-    private TextView otpPrompt;
+    private PinPadView pinpadView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
         setTitle("ENTER OTP");
+
+        pinpadView = (PinPadView) findViewById(R.id.pinpadView);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         final OtpSingleton si = OtpSingleton.getInstance();
-        continueButton = (Button) findViewById(R.id.continueButton);
-        otpEntryView = (EditText) findViewById(R.id.otpEditText);
-        otpPrompt = (TextView) findViewById(R.id.otpPrompt);
-        otpPrompt.setText(si.getOtpMessage());
-        continueButton.setOnClickListener(new View.OnClickListener() {
+
+        pinpadView.setPromptText(si.getOtpMessage());
+
+        pinpadView.setOnCompletedListener(new PinPadView.OnCompletedListener() {
 
             @Override
-            public void onClick(View view) {
+            public void onCompleted(String pin) {
                 synchronized (si) {
-                    si.setOtp(otpEntryView.getText().toString());
+                    si.setOtp(pin);
                     si.notify();
                 }
                 OtpActivity.this.finish();
