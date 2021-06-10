b59d542ec1eb84b290249e32e06d0347cc620650
Michael Obi <michael@paystack.com>
2021-06-11 00:08:36 +0100

    PaystackRepositoryImpl Test that processCardCharge calls the right service
    Signed-off-by: Michael Obi <michael@paystack.com>

--- paystack/src/test/java/co/paystack/android/api/PaystackRepositoryImplTest.kt
+++ paystack/src/test/java/co/paystack/android/api/PaystackRepositoryImplTest.kt
@@ -1,35 +1,26 @@
 package co.paystack.android.api
 
-import co.paystack.android.api.model.TransactionInitResponse
-import co.paystack.android.api.request.TransactionInitRequestBody
+import co.paystack.android.api.model.ChargeResponse
+import co.paystack.android.api.request.ChargeParams
 import co.paystack.android.api.service.PaystackApiService
-import co.paystack.android.model.Charge
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
-import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 
-@RunWith(RobolectricTestRunner::class)
 class PaystackRepositoryImplTest {
     private val apiService: PaystackApiService = mock()
 
-    @Before
-    fun setUp() {
-    }
-
     @Test
-    fun `intializeTransaction calls initializeTransaction on PaystackApiService`() {
-        whenever(apiService.initializeTransaction(TEST_TRANSACTION_INIT_BODY.toRequestMap()))
-            .thenReturn(FakeCall.success(TEST_TRANSACTION_INIT_RESPONSE))
+    fun `processCardCharge calls chargeCard on PaystackApiService once`() {
+        whenever(apiService.chargeCard(TEST_CHARGE_PARAMS.toRequestMap()))
+            .thenReturn(FakeCall.success(TEST_CHARGE_RESPONSE))
 
         val repository = PaystackRepositoryImpl(apiService)
-        repository.initializeTransaction(testPublicKey, TEST_CHARGE, testDeviceId, mock())
 
-        verify(apiService.initializeTransaction(TEST_TRANSACTION_INIT_BODY.toRequestMap()), times(1))
+        repository.processCardCharge(TEST_CHARGE_PARAMS, mock())
+        verify(apiService, times(1)).chargeCard(TEST_CHARGE_PARAMS.toRequestMap())
     }
 
     companion object {
@@ -38,25 +29,24 @@ class PaystackRepositoryImplTest {
         const val testDeviceId = "test_device_id"
         const val testAmount = 10000
         const val testCurrency = "NGN"
+        const val testTransactionId = "123458685949"
 
-        val TEST_TRANSACTION_INIT_BODY = TransactionInitRequestBody(
-            publicKey = testPublicKey,
-            email = testEmail,
-            amount = testAmount,
-            currency = testCurrency,
-            device = testDeviceId,
-            metadata = ""
+        val TEST_CHARGE_PARAMS = ChargeParams(
+            clientData = "encryptedClientData",
+            transactionId = testTransactionId,
+            last4 = "5599",
+            deviceId = testDeviceId,
+            null
         )
-        val TEST_TRANSACTION_INIT_RESPONSE = TransactionInitResponse(
+
+        val TEST_CHARGE_RESPONSE = ChargeResponse(
             status = "success",
-            transactionId = "74927849ID"
+            transactionId = testTransactionId,
+            reference = "ref_42f45g53",
+            message = "message",
+            otpMessage = "Otpmessage",
+            auth = "none",
+            countryCode = "NG"
         )
-
-        val TEST_CHARGE = Charge().apply {
-            email = testEmail
-            amount = testAmount
-            currency = testCurrency
-
-        }
     }
 }
