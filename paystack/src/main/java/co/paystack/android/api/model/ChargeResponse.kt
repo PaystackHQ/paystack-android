11254f752e3072f147ab9eec7ac546628bc00c5c
Michael Obi <michael@paystack.com>
2021-06-15 12:57:28 +0100

    Remove Json string parse helper function from ChargeResponse.kt
    Signed-off-by: Michael Obi <michael@paystack.com>

--- paystack/src/main/java/co/paystack/android/api/model/ChargeResponse.kt
+++ paystack/src/main/java/co/paystack/android/api/model/ChargeResponse.kt
@@ -2,7 +2,6 @@ package co.paystack.android.api.model
 
 
 import androidx.annotation.Keep
-import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 
 @Keep
@@ -23,22 +22,6 @@ data class ChargeResponse(
     val auth: String? = null,
 
     @SerializedName("countryCode")
-    val countryCode: String? = null,
+    val countryCode: String? = null
 
-    ) {
-
-    companion object {
-        fun fromJsonString(jsonString: String?): ChargeResponse {
-            return try {
-                Gson().fromJson(jsonString, ChargeResponse::class.java)
-            } catch (e: Exception) {
-                ChargeResponse(
-                    status = "0",
-                    transactionId = "",
-                    reference = "",
-                    message = e.message ?: "An error occurred while reading Auth data"
-                )
-            }
-        }
-    }
-}
+)
